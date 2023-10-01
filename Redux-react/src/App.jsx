fd7001438b39e300e1237c190e0e9b97660b21bb
khushal <289kksharma.01@gmail.com>
2023-10-01 17:13:29 +0530

    React-redux-todo

---
+++ Redux-react/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import AddTodo from './Components/AddTodo'
+import Todos from './Components/Todos'
+
+function App() {
+  return (
+    <>
+      React redux
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
